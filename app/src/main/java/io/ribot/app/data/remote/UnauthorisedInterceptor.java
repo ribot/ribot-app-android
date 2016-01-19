d32168660adfb71dc98a20cb656126d2f5828349
AndreiD <danandreist@gmail.com>
2016-01-19 13:45:06 +0200

    update to retrofit 2.0.0-beta3

--- app/src/main/java/io/ribot/app/data/remote/UnauthorisedInterceptor.java
+++ app/src/main/java/io/ribot/app/data/remote/UnauthorisedInterceptor.java
@@ -3,37 +3,31 @@ package io.ribot.app.data.remote;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
-
-import com.squareup.okhttp.Interceptor;
-import com.squareup.okhttp.Response;
 import com.squareup.otto.Bus;
-
 import java.io.IOException;
-
 import javax.inject.Inject;
-
 import io.ribot.app.RibotApplication;
 import io.ribot.app.data.BusEvent;
+import okhttp3.Interceptor;
+import okhttp3.Response;
 
 public class UnauthorisedInterceptor implements Interceptor {
 
-    @Inject Bus eventBus;
+  @Inject Bus eventBus;
 
-    public UnauthorisedInterceptor(Context context) {
-        RibotApplication.get(context).getComponent().inject(this);
-    }
+  public UnauthorisedInterceptor(Context context) {
+    RibotApplication.get(context).getComponent().inject(this);
+  }
 
-    @Override
-    public Response intercept(Chain chain) throws IOException {
-        Response response = chain.proceed(chain.request());
-        if (response.code() == 401) {
-            new Handler(Looper.getMainLooper()).post(new Runnable() {
-                @Override
-                public void run() {
-                    eventBus.post(new BusEvent.AuthenticationError());
-                }
-            });
+  @Override public Response intercept(Chain chain) throws IOException {
+    Response response = chain.proceed(chain.request());
+    if (response.code() == 401) {
+      new Handler(Looper.getMainLooper()).post(new Runnable() {
+        @Override public void run() {
+          eventBus.post(new BusEvent.AuthenticationError());
         }
-        return response;
+      });
     }
+    return response;
+  }
 }
